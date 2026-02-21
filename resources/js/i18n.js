f7dba48e71f278fdd8cd518bb06c5a09b932015a
sylsylviane <syl.sylviane1@gmail.com>
2026-02-21 09:00:28 -0500

    Ajuster le placement des imports par le linter

--- resources/js/i18n.js
+++ resources/js/i18n.js
@@ -1,19 +1,18 @@
-import i18n from 'i18next';
-import { initReactI18next } from 'react-i18next';
 import en from '@lang/en.json';
 import fr from '@lang/fr.json';
-    
+import i18n from 'i18next';
+import { initReactI18next } from 'react-i18next';
+
 // the translations
 // (tip move them in a JSON file and import them,
 // or even better, manage them separated from your code: https://react.i18next.com/guides/multiple-translation-files)
-
 
 i18n.use(initReactI18next) // passes i18n down to react-i18next
     .init({
         resources: {
             en: {
                 translation: en,
-            },  
+            },
             fr: {
                 translation: fr,
             },
