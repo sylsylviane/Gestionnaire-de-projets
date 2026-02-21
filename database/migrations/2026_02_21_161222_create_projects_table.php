b3e5f826939af271f894105c031ef1b43c36d72b
sylsylviane <syl.sylviane1@gmail.com>
2026-02-21 11:30:21 -0500

    Créer le fichier de migration pour la table projects

---
+++ database/migrations/2026_02_21_161222_create_projects_table.php
@@ -0,0 +1,52 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Exécuter les migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('projects', function (Blueprint $table) {
+            $table->id();
+            $table->string('project_number')->unique();
+            $table->string('project_name');
+
+            $table->enum('building_type', [
+                'combustible',
+                'incombustible_steel',
+                'incombustible_concrete'
+            ]);
+
+            $table->integer('floors');
+
+            $table->text('description')->nullable();
+
+            // Coordination technique
+            $table->boolean('electrical_done')->default(false);
+            $table->boolean('sleeves_done')->default(false);
+            $table->boolean('drainage_done')->default(false);
+
+            $table->enum('status', [
+                'en_cours',
+                'termine',
+                'suspendu'
+            ])->default('en_cours');
+
+            $table->timestamps();
+            $table->foreignId('user_id')->constrained()->onDelete('cascade');
+        });
+    }
+
+    /**
+     * Annuler les migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('projects');
+    }
+};
