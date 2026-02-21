ee837f0df2d876bdbab2de88c49a95956b93c193
sylsylviane <syl.sylviane1@gmail.com>
2026-02-21 14:36:54 -0500

    Créer la route pour afficher les projets

--- routes/web.php
+++ routes/web.php
@@ -3,6 +3,7 @@
 use Illuminate\Support\Facades\Route;
 use Inertia\Inertia;
 use Laravel\Fortify\Features;
+use App\Http\Controllers\ProjectController;
 
 Route::get('/', function () {
     return Inertia::render('welcome', [
@@ -13,5 +14,9 @@ Route::get('/', function () {
 Route::get('dashboard', function () {
     return Inertia::render('dashboard');
 })->middleware(['auth', 'verified'])->name('dashboard');
+
+Route::get('/projects', [ProjectController::class, 'index'])
+    ->middleware(['auth', 'verified'])
+    ->name('projects.index');
 
 require __DIR__.'/settings.php';
