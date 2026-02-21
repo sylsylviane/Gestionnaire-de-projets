63f92fb1a97ff8092ecb06583dc2c7a0873df8e3
sylsylviane <syl.sylviane1@gmail.com>
2026-02-21 09:09:36 -0500

    Ajouter des traductions

--- lang/fr/passwords.php
+++ lang/fr/passwords.php
@@ -13,10 +13,10 @@ return [
     |
     */
 
-    "reset" => "Votre mot de passe a été réinitialisé !",
-    "sent" => "Nous vous avons envoyé par mail le lien de réinitialisation du mot de passe !",
+    'reset' => 'Votre mot de passe a été réinitialisé !',
+    'sent' => 'Nous vous avons envoyé par mail le lien de réinitialisation du mot de passe !',
     'throttled' => 'Veuillez patienter avant de réessayer.',
-    "token" => "Ce jeton de réinitialisation du mot de passe n'est pas valide.",
-    "user" => "Aucun utilisateur n'a été trouvé avec cette adresse e-mail.",
+    'token' => "Ce jeton de réinitialisation du mot de passe n'est pas valide.",
+    'user' => "Aucun utilisateur n'a été trouvé avec cette adresse e-mail.",
 
 ];
