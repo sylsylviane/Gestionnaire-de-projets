1760d5985d169f3ddc7ace938e0dcc90ed3355b2
sylsylviane <syl.sylviane1@gmail.com>
2026-02-21 14:33:01 -0500

    Créer le modèle pour la table projects

---
+++ app/Models/Project.php
@@ -0,0 +1,36 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Relations\BelongsToMany;
+
+class Project extends Model
+{
+    protected $fillable = [
+        'project_name',
+        'project_number',
+        'building_type',
+        'floors',
+        'description',
+        'electrical_done',
+        'sleeves_done',
+        'drainage_done',
+        'status',
+        'created_at',
+        'updated_at',
+        'user_id'
+    ];
+
+    /** 
+     * Retourne les utilisateurs (dessinateurs) associés à ce projet.
+     * Relation : un projet peut être assigné à plusieurs utilisateurs.
+     * @return \Illuminate\Database\Eloquent\Relations\BelongsToMany
+     */
+    public function users() : BelongsToMany
+    {
+        return $this->belongsToMany(User::class)
+            ->withPivot('role')
+            ->withTimestamps();
+    }
+}
